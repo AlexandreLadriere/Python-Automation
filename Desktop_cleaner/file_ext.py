416449ab527d7ba74f830548f6c05b0b3f647b65
AlexandreLadriere <alexandre.ladriere77@gmail.com>
2020-01-07 19:13:21 +0100

    upd: Updating file_ext.py by adding dictionnary

--- Desktop_cleaner/file_ext.py
+++ Desktop_cleaner/file_ext.py
@@ -1,14 +1,206 @@
-EXT_AUDIO = ['.aif', '.cda', '.mid', '.midi', '.mp3', '.mpa', '.ogg', '.wav', '.wma', '.wpl', '.m3u', '.flp']
-EXT_TEXT = ['.txt', '.doc', '.docx', '.odt', '.pdf', '.rtf', '.tex', '.wks', '.wps', '.wpd']
-EXT_VIDEO = ['.3g2', '.3gp', '.avi', '.flv', '.h264', '.m4v', '.mkv', '.mov', '.mp4', '.mpg', '.mpeg', '.rm', '.swf', '.vob', '.wmv']
-EXT_IMAGE = ['.ai', '.bmp', '.gif', '.ico', '.jpg', '.jpeg', '.png', '.ps', '.psd', '.svg', '.tif', '.tiff', '.CR2']
-EXT_INTERNET = ['.asp', '.aspx', '.cer', '.cfm', '.cgi', '.pl', '.css', '.htm', '.js', '.jsp', '.part', '.php', '.rss', '.xhtml']
-EXT_COMPRESSED = ['.7z', '.arj', '.deb', '.pkg', '.rar', '.rpm', '.tar', '.gz', '.z', '.zip']
-EXT_DISC = ['.bin', '.dmg', '.iso', '.toast', '.vcd']
-EXT_DATA = ['.csv', '.dat', '.db', '.dbf', '.log', '.mdb', '.sav', '.sql', '.tar', '.xml', '.json']
-EXT_EXEC = ['.apk', '.bat', '.com', '.exe', '.gadget', '.jar', '.wsf']
-EXT_FONTS = ['.fnt', '.fon', '.otf', '.ttf']
-EXT_PRES = ['.key', '.odp', '.pps', '.ppt', '.pptx']
-EXT_PROG = ['.c', '.class', '.cpp', '.h', '.java', '.javac', '.kt', '.py', '.dart', '.sh', '.html', '.php', '.go', '.swift', '.cs', '.vb', '.bat', '.cmd', '.js', '.ts', '.pl', '.r']
-EXT_SHEETS = ['.ods', '.xlr', '.xls', '.xlsx']
-EXT_SYS = ['.bak', '.cab', '.cfg', '.cpl', '.cur', '.dll', '.dmp', '.drv', '.icns', '.ico', '.ini', '.lnk', '.msi', '.sys', '.tmp']
+EXT_AUDIO = [
+    '.aif', 
+    '.cda', 
+    '.mid', 
+    '.midi', 
+    '.mp3', 
+    '.mpa', 
+    '.ogg', 
+    '.wav', 
+    '.wma', 
+    '.wpl', 
+    '.m3u', 
+    '.flp'
+    ]
+
+EXT_TEXT = [
+    '.txt', 
+    '.doc', 
+    '.docx', 
+    '.odt', 
+    '.pdf', 
+    '.rtf', 
+    '.tex', 
+    '.wks', 
+    '.wps', 
+    '.wpd'
+    ]
+
+EXT_VIDEO = [
+    '.3g2', 
+    '.3gp', 
+    '.avi', 
+    '.flv', 
+    '.h264', 
+    '.m4v', 
+    '.mkv', 
+    '.mov', 
+    '.mp4', 
+    '.mpg', 
+    '.mpeg', 
+    '.rm', 
+    '.swf', 
+    '.vob', 
+    '.wmv'
+    ]
+
+EXT_IMAGE = [
+    '.ai', 
+    '.bmp', 
+    '.gif', 
+    '.ico', 
+    '.jpg', 
+    '.jpeg', 
+    '.png', 
+    '.ps', 
+    '.psd', 
+    '.svg', 
+    '.tif', 
+    '.tiff', 
+    '.CR2'
+    ]
+
+EXT_INTERNET = [
+    '.asp', 
+    '.aspx', 
+    '.cer', 
+    '.cfm', 
+    '.cgi', 
+    '.pl', 
+    '.css', 
+    '.htm', 
+    '.js', 
+    '.jsp', 
+    '.part', 
+    '.php', 
+    '.rss', 
+    '.xhtml'
+    ]
+
+EXT_COMPRESSED = [
+    '.7z', 
+    '.arj', 
+    '.deb', 
+    '.pkg', 
+    '.rar', 
+    '.rpm', 
+    '.tar', 
+    '.gz', 
+    '.z', 
+    '.zip'
+    ]
+
+EXT_DISC = [
+    '.bin', 
+    '.dmg', 
+    '.iso', 
+    '.toast', 
+    '.vcd'
+    ]
+
+EXT_DATA = [
+    '.csv', 
+    '.dat', 
+    '.db', 
+    '.dbf', 
+    '.log', 
+    '.mdb', 
+    '.sav', 
+    '.sql', 
+    '.tar', 
+    '.xml', 
+    '.json'
+    ]
+
+EXT_EXEC = [
+    '.apk', 
+    '.bat', 
+    '.com', 
+    '.exe', 
+    '.gadget', 
+    '.jar', 
+    '.wsf'
+    ]
+
+EXT_FONTS = [
+    '.fnt', 
+    '.fon', 
+    '.otf', 
+    '.ttf'
+    ]
+
+EXT_PRES = [
+    '.key', 
+    '.odp', 
+    '.pps', 
+    '.ppt', 
+    '.pptx'
+    ]
+
+EXT_PROG = [
+    '.c', 
+    '.class', 
+    '.cpp', 
+    '.h', 
+    '.java', 
+    '.javac', 
+    '.kt', 
+    '.py', 
+    '.dart', 
+    '.sh', 
+    '.html', 
+    '.php', 
+    '.go', 
+    '.swift', 
+    '.cs', 
+    '.vb', 
+    '.bat', 
+    '.cmd', 
+    '.js', 
+    '.ts', 
+    '.pl', 
+    '.r'
+    ]
+
+EXT_SHEETS = [
+    '.ods', 
+    '.xlr', 
+    '.xls', 
+    '.xlsx'
+    ]
+
+EXT_SYS = [
+    '.bak', 
+    '.cab', 
+    '.cfg', 
+    '.cpl', 
+    '.cur', 
+    '.dll', 
+    '.dmp', 
+    '.drv', 
+    '.icns', 
+    '.ico', 
+    '.ini', 
+    '.lnk', 
+    '.msi', 
+    '.sys', 
+    '.tmp'
+    ]
+
+EXT = {
+    'AUDIO': EXT_AUDIO, 
+    'TEXT': EXT_TEXT, 
+    'VIDEO': EXT_VIDEO, 
+    'IMAGE': EXT_IMAGE, 
+    'INTERNET': EXT_INTERNET, 
+    'COMPRESSED': EXT_COMPRESSED, 
+    'DISC': EXT_DISC, 
+    'DATA': EXT_DATA, 
+    'EXEC': EXT_EXEC, 
+    'FONTS': EXT_FONTS, 
+    'PRES': EXT_PRES, 
+    'PROG': EXT_PROG, 
+    'SHEETS': EXT_SHEETS, 
+    'SYS': EXT_SYS,
+    'OTERS': []
+    }
